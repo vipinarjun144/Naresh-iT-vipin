60cf4a739c84a702d8a7c6b06fea123b2788b608
vipin <vipin@vipin-PC>
2020-02-09 00:45:35 +0530

    p1

---
+++ src/gitProject/Gitpract.java
@@ -0,0 +1,17 @@
+package gitProject;
+
+public class Gitpract {
+	private String sname;
+	private Integer pid;
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+}
+
+
+
